e124902385399a6e8c6cf905090cded7a99ae19d
Dakota <bazookaharwell@gmail.com>
2017-03-14 16:57:32 -1000

    I may or may not have forgotten to fork

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,17 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function secretNumberGenerator() {
+
+  var secretNumberValue = _createSecretNumber();
+
+  // you do know it is possible to get the same random number 3 times in a row, but if that happens I should quit this course and buy a lottery ticket.
+  function _createSecretNumber() {
+    return Math.floor(Math.random() * 1000001);
+  }
+
+  function getSecretNumber() {
+    return secretNumberValue;
+  }
+
+  return getSecretNumber;
+
 };
